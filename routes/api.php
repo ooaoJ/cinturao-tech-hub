dd83035c6aa2484ed6588cb0c6618542b594ef80
João Pedro <jpbentosperandio@gmail.com>
2026-04-14 12:06:31 -0300

    Commit: update routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\EquipeController;
 use App\Http\Controllers\Api\OrientadorController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ProjetoController;
+use App\Http\Controllers\Api\TesteController;
+use App\Http\Controllers\Api\DocumentoProjetoController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login'])->name('login');
@@ -10,7 +14,34 @@ Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::middleware('auth:api')->group(function () {
     Route::get('/me', [AuthController::class, 'me']);
     Route::post('/logout', [AuthController::class, 'logout']);
-    Route::get('/refresh', [AuthController::class, 'refresh']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
 
-    Route::post('/accept', [OrientadorController::class, 'aprovar']);
+    Route::prefix('/usuarios')->group(function () {
+        Route::get('/pendentes', [OrientadorController::class, 'pendentes']);
+        Route::get('/aprovados', [OrientadorController::class, 'aprovados']);
+        Route::post('/aprovar', [OrientadorController::class, 'aprovar']);
+        Route::post('/reprovar', [OrientadorController::class, 'reprovar']);
+    });
+
+    Route::prefix('equipes')->group(function () {
+        Route::get('/', [EquipeController::class, 'index']);
+        Route::get('/{id}', [EquipeController::class, 'show']);
+        Route::post('/', [EquipeController::class, 'store']);
+        Route::post('/integrantes', [EquipeController::class, 'adicionarIntegrante']);
+        Route::delete('/integrantes/{id}', [EquipeController::class, 'removerIntegrante']);
+    });
+
+    Route::prefix('projetos')->group(function () {
+        Route::get('/', [ProjetoController::class, 'index']);
+        Route::get('/{id}', [ProjetoController::class, 'show']);
+        Route::post('/', [ProjetoController::class, 'store']);
+        Route::put('/{id}', [ProjetoController::class, 'update']);
+        Route::post('/{id}/aprovar', [ProjetoController::class, 'aprovar']);
+
+        Route::get('/{projetoId}/testes', [TesteController::class, 'index']);
+        Route::post('/{projetoId}/testes', [TesteController::class, 'store']);
+
+        Route::get('/{projetoId}/documentos', [DocumentoProjetoController::class, 'index']);
+        Route::post('/{projetoId}/documentos', [DocumentoProjetoController::class, 'store']);
+    });
 });
